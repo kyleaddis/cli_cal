e29d3780f6ea8bb9bb93597b20a3c887471e6890
Kyle Addis <kyle.a.addis@gmail.com>
2024-11-16 20:47:06 +0530

    added tests

--- tests/test_leap_year.py
+++ tests/test_leap_year.py
@@ -4,5 +4,7 @@ from cli_cal.cal import Calendar
 def test_leap_year():
     y = Calendar(2024)
     y1 = Calendar(2023)
+    y2 = Calendar(2000)
     assert y.is_leap_year == True
     assert y1.is_leap_year == False
+    assert y2.is_leap_year == True
